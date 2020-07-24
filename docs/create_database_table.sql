38e496e3ebca0a380366e412df8fa1bfd6a18ca2
sigma <sigma@localhost.localdomain>
2020-07-24 17:09:45 +0530

    Rough Datbase Design added

---
+++ docs/create_database_table.sql
@@ -0,0 +1,20 @@
+create database rakhtacharitra;
+
+create table donor (
+    
+    id int not null auto_increment primary key,
+    fullname varchar(100) not null,
+    username varchar(100) not null,
+    password varchar(100) not null,
+    email varchar(100),
+    phone bigint(15),
+    dob varchar(100),
+    bloodgroup varchar(10),
+    gender varchar(10) not null,
+    state varchar(100) not null,
+    pincode bigint(10) not null,
+    city varchar(100) not null,
+    area varchar(100) not null
+
+);
+
---
+++ docs/insert_dummy_data.sql
@@ -0,0 +1,6 @@
+-- Donor Table Data
+insert into donor values(1,"abhijeet sharma","iamabs","123","abhijeetsharma248@gmail.com","9004869069","20 April 2001","B+","Male","Maharashtra","400061","mumbai","malad");
+insert into donor values(2,"George smith","george","123","geroge@gmail.com","986283434","23 June 2006","A-","Male","Bihar","808080","patna","rajnagar");
+insert into donor values(3,"sonu singh","sonu","123","sonu@gmail.com","934534523","12 December 1990","A+","Female","Maharashtra","400042","mumbai","virar");
+insert into donor values(4,"smiriti kulkarni","smiriti","123","smiriti@gmail.com","823423443","22 March 1780","O+","Female","Delhi","434352","Delhi","noida");
+insert into donor values(5,"Raja das","raja","123","rajadas@gmail.com","723842342","03 July 2005","O-","Male","Kolkata","345334","howrah","hind");
