f4a739bb468ad253bf33e1f84a9cb4b52af6ebfa
sigma <sigma@localhost.localdomain>
2020-07-24 15:52:13 +0530

    Client App Added from /sigmanpo/bloodbank

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Home page 
 router.get('/', function(req, res, next) {
-  res.send("Hello world");
+  res.render('index',{title:'The Online Blood Bank'});
 });
-
+// Home Page
+router.get('/home', function(req, res, next) {
+  res.render('index',{title:'The Online Blood Bank'});
+});
+// Donor page
+router.get('/donor', function(req, res, next) {
+  res.render('donor',{title:'Donor'});
+});
+// Search Donor
+router.get('/search', function(req, res, next) {
+  res.render('search',{title:'Search Donor'});
+});
 module.exports = router;
