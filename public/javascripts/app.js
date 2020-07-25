89aba6bd37eb00728a244adb47645dd20bd4fdcd
sigma <sigma@localhost.localdomain>
2020-07-25 19:00:38 +0530

    Login & Logout Implemented in Client Side

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,44 @@
-function toggleNavBar() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
+const BASE_URL = "http://localhost:3000/donor";
+
+const loginBtn = document.getElementById("login");
+const logoutBtn = document.getElementById("logout");
+
+const loginusername = document.getElementById("loginusername");
+const loginpassword = document.getElementById("loginpassword");
+
+loginBtn.addEventListener('click',() => {
+  login(loginusername.value,loginpassword.value);
+});
+
+logoutBtn.addEventListener('click',() => {
+  logout();
+});
+
+function login(username,password) {
+  fetch(BASE_URL+"/signin",
+    {
+      method:"POST",
+      body:JSON.stringify({"username":username,"password":password}),
+      headers: { "Content-type": "application/json"} 
+    })
+    .then(response => response.json()) 
+    .then((data) => {
+      if(data.success == true) 
+        window.location.href = "../../donor";
+      else 
+        alert('Username or Password Invalid !')
+    }); 
   }
+
+function logout() {
+  window.location.href = "../../donor/logout";
+}
+
+function toggleNavBar() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
